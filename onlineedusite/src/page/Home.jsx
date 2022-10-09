0e3ca83db9481d258cb65bbc52ef5bde113a6356
Md Faisal Raza Khan <irkfaisal@gmail.com>
2022-10-09 19:35:29 +0530

    company banner section

--- onlineedusite/src/page/Home.jsx
+++ onlineedusite/src/page/Home.jsx
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 
 import Header from '../components/Header/Header';
 import HeroSection from '../components/Hero-Section/Hero-section';
+import CompanySection from '../components/Company-section/company-section';
 
 const Home = () => {
     return (
         <Fragment>
            <Header></Header>
            <HeroSection></HeroSection>
+           <CompanySection></CompanySection>
         </Fragment>
     )
 }
