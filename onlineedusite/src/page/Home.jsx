96b045c87d667bb80caea6e2fc8fc6466acb6e39
Md Faisal Raza Khan <irkfaisal@gmail.com>
2022-10-09 19:23:49 +0530

    Banner section

--- onlineedusite/src/page/Home.jsx
+++ onlineedusite/src/page/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 
 import Header from '../components/Header/Header';
+import HeroSection from '../components/Hero-Section/Hero-section';
 
 const Home = () => {
     return (
         <Fragment>
            <Header></Header>
+           <HeroSection></HeroSection>
         </Fragment>
     )
 }
