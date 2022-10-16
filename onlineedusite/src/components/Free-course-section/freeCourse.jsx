f1c06da8c968a8c425f4dc49dada713d11351b92
Md Faisal Raza Khan <irkfaisal@gmail.com>
2022-10-16 18:45:46 +0530

    free course section work

--- onlineedusite/src/components/Free-course-section/freeCourse.jsx
+++ onlineedusite/src/components/Free-course-section/freeCourse.jsx
@@ -5,8 +5,9 @@ import courseImg01 from "../../assests/images/web-development.png";
 import courseImg02 from "../../assests/images/kids-learning.png";
 import courseImg03 from "../../assests/images/seo.png";
 import courseImg04 from "../../assests/images/ui-ux.png";
+import FreeCourseCard from "./FreeCourseCard";
 
-import "./freeCourse.css";
+import "./free-course.css";
 
 const freeCourseData = [
   {
